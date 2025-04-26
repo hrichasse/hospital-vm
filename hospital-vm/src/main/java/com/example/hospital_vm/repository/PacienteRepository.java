bf3e2ed9ad57b9958b32a6fe346fb15371d59613
hernan <hernanrichasse@gmail.com>
2025-04-26 12:32:52 -0400

    se agrego Repositorio

---
+++ hospital-vm/src/main/java/com/example/hospital_vm/repository/PacienteRepository.java
@@ -0,0 +1,13 @@
+package com.example.hospital_vm.repository;
+
+
+import com.example.hospital_vm.model.Paciente;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PacienteRepository extends JpaRepository <Paciente,Integer> {
+
+}
